3a5927ce00b029aba515ee3cf48fdc7b51b84960
Memet <exortek979@gmail.com>
2024-05-28 12:50:48 +0300

    Create orders.js

---
+++ orders.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        id: 1,
+        customer: 'John Doe',
+        product: 'Pizza Margherita',
+    },
+    {
+        id: 2,
+        customer: 'Jane Smith',
+        product: 'Spaghetti Carbonara',
+    },
+    {
+        id: 3,
+        customer: 'Mike Johnson',
+        product: 'Cesar Salad',
+    },
+    {
+        id: 4,
+        customer: 'Anna Brown',
+        product: 'Tiramisu',
+    }
+]
