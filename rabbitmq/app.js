ec941e6ae15848ea693fb99e2396ec998df2eeca
Memet <exortek979@gmail.com>
2024-05-28 12:50:51 +0300

    added scenario

--- rabbitmq/app.js
+++ rabbitmq/app.js
@@ -1,44 +1,49 @@
-const express = require('express');
-const app = express();
-const amqp = require('amqplib/callback_api');
+const amqp = require('amqplib');
+const orders = require('../orders');
+const {pause} = require('../helpers');
 
-let channel = null;
-const queue = 'hello';
-amqp.connect('amqp://localhost', (error0, connection) => {
-    if (error0) {
-        throw error0;
+const queueName = 'orderQueue';
+const amqpUrl = 'amqp://localhost';
+
+let channel;
+
+amqp.connect(amqpUrl).then(async (connection) => {
+    channel = await connection.createChannel();
+    await channel.assertQueue(queueName, {durable: true});
+
+    console.info('RabbitMQ connected successfully!');
+    await createOrders();
+    await processOrders();
+
+    await channel.close();
+    await connection.close();
+    console.info('RabbitMQ connection closed, all tasks completed.');
+    process.exit(0);
+}).catch((err) => {
+    console.error('RabbitMQ connection error', err);
+    if (channel) channel.close();
+    process.exit(1);
+});
+
+const createOrders = async () => {
+    for (const order of orders) {
+        console.log(` [x] ${order.product} order received from ${order.customer}`);
+        await channel.sendToQueue(queueName, Buffer.from(JSON.stringify(order)), {persistent: true});
+        await pause(1000);
     }
-    connection.createChannel((error1, ch) => {
-        if (error1) {
-            throw error1;
+};
+
+const processOrders = async () => {
+    while (true) {
+        const msg = await channel.get(queueName, {noAck: false});
+        if (msg) {
+            const parsedOrder = JSON.parse(msg.content.toString());
+            console.log(` [x] The order was completed and delivered to ${parsedOrder.customer}`);
+            channel.ack(msg);
+            await pause(3000);
+        } else {
+            console.log('No orders left');
+            break;
         }
-        channel = ch;
-        channel.assertQueue(queue, {
-            durable: false
-        }, (error2, ok) => {
-            if (error2) {
-                throw error2;
-            }
-            console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', queue);
-            channel.consume(queue, (msg) => {
-                    console.log(" [x] Received %s", msg.content.toString());
-                }, {
-                    noAck: true
-                }
-            );
-        });
-    });
-});
-
-
-app.get('/send', (req, res) => {
-    const msg = 'Hello World! This is a message from the sender!';
-    channel.sendToQueue(queue, Buffer.from(msg));
-    console.log(" [x] Sent %s", msg);
-    res.send('Message sent');
-});
-
-
-app.listen(5001, () => {
-    console.info(`Server started http://localhost:5001`)
-});
+    }
+};
