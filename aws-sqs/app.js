3d5545b0639fe08a2c5258e0c4ab34b3a504b53e
Memet <exortek979@gmail.com>
2024-05-28 12:50:43 +0300

    added scenario

--- aws-sqs/app.js
+++ aws-sqs/app.js
@@ -1,51 +1,74 @@
-const express = require('express');
-const app = express();
-const {SQSClient, SendMessageCommand, ReceiveMessageCommand} = require('@aws-sdk/client-sqs');
+const {SQSClient, SendMessageCommand, ReceiveMessageCommand, DeleteMessageCommand} = require('@aws-sdk/client-sqs');
+const orders = require('../orders');
+const {pause} = require('../helpers');
 
-
+const region = 'YOUR_REGION';
 const config = {
-    region: 'eu-west-1',
-    accessKeyId: 'accessKeyId',
-    secretAccessKey:'secretAccessKey',
-    queueUrl: 'https://sqs.eu-west-1.amazonaws.com/123456789012/MyQueue'
+    region: region,
+    credentials: {
+        accessKeyId: 'YOUR_ACCESS_KEY_ID',
+        secretAccessKey: 'YOUR_SECRET_ACCESS_KEY'
+    },
+    messageGroupId: 'ORDER_GROUP_ID',
+    queueUrl: `https://sqs.${region}.amazonaws.com/ACCOUNT_ID/ORDER_QUEUE_NAME`
 }
-
 
 const sqsClient = new SQSClient({
     region: config.region,
-    credentials: {
-        accessKeyId: config.accessKeyId,
-        secretAccessKey: config.secretAccessKey
-    }
+    credentials: config.credentials
 });
 
-app.get('/send', async (req, res) => {
-    const message = 'Hello AWS SQS! This is a message from the sender!';
-    const params = {
-        QueueUrl: config.queueUrl,
-        MessageBody: message
-    };
-    await sqsClient.send(new SendMessageCommand(params));
-    console.log(' [x] Sent message: ', message);
-    res.send('Message sent');
-});
+const createOrders = async () => {
+    for (const order of orders) {
+        console.log(` [x] ${order.product} order received from ${order.customer}`);
 
-app.get('/receive', async (req, res) => {
         const params = {
             QueueUrl: config.queueUrl,
-            MaxNumberOfMessages: 1
+            MessageBody: JSON.stringify(order),
+            DelaySeconds: 0,
+            MessageDeduplicationId: order.id.toString(),
+            MessageGroupId: config.messageGroupId
         };
+
+        await sqsClient.send(new SendMessageCommand(params));
+
+        await pause(1000);
+    }
+};
+
+const processOrders = async () => {
+    while (true) {
+        const params = {
+            QueueUrl: config.queueUrl,
+            MaxNumberOfMessages: 1,
+            WaitTimeSeconds: 10
+        };
+
         const data = await sqsClient.send(new ReceiveMessageCommand(params));
-        if (data.Messages) {
-            console.log(' [x] Received message: ', data.Messages[0].Body);
-            res.send(data.Messages[0].Body);
+
+        if (data.Messages && data.Messages.length > 0) {
+            const message = data.Messages[0];
+            const parsedOrder = JSON.parse(message.Body);
+            console.log(` [x] The order was completed and delivered to ${parsedOrder.customer}`);
+
+            const deleteParams = {
+                QueueUrl: config.queueUrl,
+                ReceiptHandle: message.ReceiptHandle
+            };
+            await sqsClient.send(new DeleteMessageCommand(deleteParams));
+
+            await pause(3000);
         } else {
-            console.log('No messages available');
-            res.send('No messages available');
+            console.log('No orders left');
+            break;
         }
     }
-);
+};
 
-app.listen(5001, () => {
-    console.info(`Server started http://localhost:5001`)
-});
+(async () => {
+    console.info('AWS SQS connected successfully!');
+    await createOrders();
+    await processOrders();
+    console.info('AWS SQS tasks completed.');
+    process.exit(0);
+})();
